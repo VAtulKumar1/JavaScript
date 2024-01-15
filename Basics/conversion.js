fe5c424e161cffae323e4584be816cda7982141c
VAtulKumar1 <atulkumarverma304@gmail.com>
2024-01-15 23:27:06 +0530

    learn conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,19 @@
+let age = 40
+let stringAge = String(age)
+console.log(stringAge)
+console.log(typeof stringAge);
+let booleanAge = Boolean(age)
+console.log(booleanAge)
+console.log(typeof booleanAge);
+let bigIntAge = BigInt(age)
+console.log(bigIntAge)
+console.log(typeof bigIntAge);
+
+
+let isNumber = "0a"
+let isNumberNumber = Number(isNumber)
+console.log(typeof isNumberNumber);
+console.log(isNumberNumber);
+
+
+// "0a" to number => NaN

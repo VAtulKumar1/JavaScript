84e7677d9a9741b81ae74904dad789ba468712ca
VAtulKumar1 <atulkumarverma304@gmail.com>
2024-01-15 22:28:53 +0530

    learn variables

---
+++ Basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = "19191"
+let accountEmail = "atul@gmail.com"
+var accountPassword = "System123#"
+accountCity = "Noida"
+
+// accountId = "1222"
+/*
+Prefer not to use var
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
